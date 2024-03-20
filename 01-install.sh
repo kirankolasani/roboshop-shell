b6e91efd50fa31796e0034a1b6bfa6a4dbf05541
kirankolasani <kirankumark906@gmail.com>
2024-03-20 15:45:47 +0530

    new update

--- 01-install.sh
+++ 01-install.sh
@@ -4,9 +4,9 @@ USER=$(id -u)
 DATE=$(date +%F)
 LogFile=/tmp/$0-$DATE.log
 
-if ( $USER -eq 0 )
+if [ $USER -eq 0 ]
 then 
  echo -e "\e[31m its root user \e[0m"
 else
  echo -e "\e[32m it's not root user"
- fi
+fi
