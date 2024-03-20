dcca2355f6fdfd11accddc2f5cb123f5fd6ac4d8
kirankolasani <kirankumark906@gmail.com>
2024-03-20 16:07:42 +0530

    new update

--- 02-add.sh
+++ 02-add.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-a=25
-b=34
+# a=25
+# b=34
+
+a=$1
+b=$2
 
 add=$((a+b))
 
-echo "Output is : $add"
+echo "Output is : $add" 
+echo "plz print all args: $@"
+echo "Plz print args count: $#"
