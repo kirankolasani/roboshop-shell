5318e66401c25402e229d8d0c6a9adc36da76c3b
kirankolasani <kirankumark906@gmail.com>
2024-03-23 20:54:20 +0530

    ec2 script

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INSTANCE_NAME=("web" "mongodb" "redis" "mysql" "catalogue" "user" "cart" "shipping" "rabbitmq" "payment" "dispatch")
-INSTANCE_TYPE="t3.micro"
+INSTANCE_TYPE=""
 SECURITY_group_ID="sg-0fa491629b46961b3"
 IMAGE_ID="ami-0f3c7d07486cad139"
 
@@ -10,7 +10,8 @@ do
   if [[ $i=="mongodb" || $i=="mysql" ]]
   then
    INSTANCE_TYPE="t2.medium"
-   
+  else
+   INSTANCE_TYPE="t3.micro"
   fi
   
   IPADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_group_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$i'}]'|jq -r '.Instances[0].PrivateIpAddress')
