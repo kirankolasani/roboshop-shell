59190b4606c4bab9e0f220d4868d5e7742611ea5
kirankolasani <kirankumark906@gmail.com>
2024-03-23 20:22:18 +0530

    ec2 script

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 INSTANCE_NAME=("web" "mongodb" "redis" "mysql" "catalogue" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "dispatch")
-
+INSTANCE_TYPE=""
+SECURITY_group_ID="sg-0fa491629b46961b3"
+IMAGE_ID="ami-0f3c7d07486cad139"
 
 for i in ${INSTANCE_NAME[@]}
 do 
- echo "creating instance $i"
+  if [[$i=="mongodb" || $i=="mysql"]]
+  then
+   INSTANCE_TYPE="t2.medium"
+   echo "creating instance $i"
+   
+  fi
+  INSTANCE_TYPE="t2.micro"
+  aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_group_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$i'}]'
+  
 done
 
 #aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-0fa491629b46961b3 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=web}]'
